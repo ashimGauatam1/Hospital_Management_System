a812d3e806b3e2fef7ca4170a7277ae661fb6859
ramchandraGautam1 <ramchandra.nesdo@gmail.com>
2024-07-25 10:13:49 +0545

    payment

--- Hospital_M_S/src/Components/Checkoutform.jsx
+++ Hospital_M_S/src/Components/Checkoutform.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from '../Routes/Payment';
+import image from '../assets/img/cpay.png'
 
 const stripePromise = loadStripe('pk_test_51PgHUWLBc8kTBo8RMCvdQmWfrofZvNxav4YBpNb1Se5YucJTFex5SFUD1vqziXboADm25dClhzBk3nFoF1yZwjXt00qUdCj0Uv');
 
 const Payment = () => (
-  <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12">
-    <div className="max-w-lg w-full">
-      <h2 className="text-3xl font-bold text-center text-gray-700 mb-6">Payment Page</h2>
-      <Elements stripe={stripePromise}>
-        <CheckoutForm />
-      </Elements>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12">
+    <div className="flex items-center space-x-8">
+      <img 
+        className='w-1/2 max-w-[1000px] h-auto object-cover' 
+        src={image} 
+        alt="Payment Illustration"
+      />
+      <div className="max-w-lg w-full">
+        <h2 className="text-3xl font-bold text-center text-cyan-800 mb-6">Payment Page</h2>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </div>
     </div>
   </div>
+  
 );
 
 export default Payment;
